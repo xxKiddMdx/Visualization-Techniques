c0f224518637883546601bd8168d5412bbf9a03b
Glitch (miniproject1-vis) <none>
2023-02-24 06:18:11 +0000

    commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector("button"); // Get the button from the page
+// Detect clicks on the button
+if (btn) {
+  btn.onclick = function() {
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn.classList.toggle("dipped");
+  };
+}
+
+// This is a single line JS comment
+/*
+This is a comment that can span multiple lines 
+- use comments to make your own notes!
+*/
